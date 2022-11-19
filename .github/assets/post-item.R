1f701e34cec1b838488525ba82528b8105a91fc0
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-11-19 17:10:28 +0100

    ci: use rtoot
    Fixes #191

--- .github/assets/post-item.R
+++ .github/assets/post-item.R
@@ -38,19 +38,6 @@ items <- c(
     paste("From #AwesomeQuarto:", unlist(items[!sapply(items, is.null)], use.names = FALSE)),
     "#QuartoPub",
     sep = "\n"
-  ),
-  paste(
-    "Any #QuartoPub (@quarto_pub) questions/issues/ideas? There is only one place!",
-    "Take a look at the Issues and Discussions, it's a gold mine of tips, tricks, hacks, solutions, and more.",
-    "https://github.com/quarto-dev/quarto-cli",
-    sep = "\n"
-  ),
-  paste(
-    "You saw or you made something related to #QuartoPub (@quarto_pub)?",
-    "You want it to be seen/shared?",
-    "Please submit a suggestion issue to #AwesomeQuarto (https://github.com/mcanouil/awesome-quarto)",
-    "PS: if you like this list, star it and share it 😉",
-    sep = "\n"
   )
 )
 items <- items[!grepl("quarto-journals", items)]
@@ -64,19 +51,29 @@ items[which(over_char > 0)] <- mapply(
   x = sprintf(".{%s}\n#QuartoPub$", over_char[which(over_char > 0)] + 4),
   y = items[which(over_char > 0)]
 )
-set.seed(as.numeric(format(Sys.Date(), "%Y%m")))
-items <- items[seq_along(items) %% 31 %in% as.numeric(format(Sys.Date(), "%d"))]
+set.seed(as.numeric(format(Sys.Date(), "%Y%m%d")))
 item <- sample(items, 1)
+
 library(rtweet)
-auth <- rtweet_bot(
+library(rtoot)
+auth <- rtweet::rtweet_bot(
   api_key = Sys.getenv("TWITTER_API_KEY"),
   api_secret = Sys.getenv("TWITTER_API_KEY_SECRET"),
   access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
   access_secret = Sys.getenv("TWITTER_ACCESS_SECRET")
 )
-auth_as(auth)
+rtweet::auth_as(auth)
+# rtoot_token <- rtoot:::get_token_from_envvar()
+
+
 i <- 0
-while(inherits(try(post_tweet(status = paste("🤖", item)), silent = TRUE), "try-error") & i < 3) {
+while(inherits(try(rtweet::post_tweet(status = paste("🤖", item))), "try-error") & i < 3) {
+  message(sprintf("Try: %s", i))
+  i <- i + 1
+}
+
+i <- 0
+while(inherits(try(rtoot::post_toot(status = paste("🤖", item))), "try-error") & i < 3) {
   message(sprintf("Try: %s", i))
   i <- i + 1
 }
